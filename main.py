99b6d3f38ce913b1abeb6909c03af46e49c716d7
Noah <noahmayanrd96@gmail.com>
2025-09-08 20:49:52 -0400

    Added optional command line flag '--verbose' that includes token cost information in response.

--- main.py
+++ main.py
@@ -2,29 +2,51 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
 
 def main():
+    load_dotenv()
 
-    arguments = sys.argv
-    prompt = arguments[1]
+    verbose = "--verbose" in sys.argv
+    args = sys.argv[1:]
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
-    print(prompt)
-
-    if len(arguments) < 1:
-        print("ERROR: Please include command line argument to use as prompt")
+    if not args:
+        print("Ai Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
+    user_prompt = " ".join(args)
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=prompt
+        model="gemini-2.0-flash-001",
+        contents=messages,
     )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
 
 if __name__ == "__main__":
     main()
