beb3fd41dbb84aca61e21ddd9554dc2dcdc6dfad
Noah <noahmayanrd96@gmail.com>
2025-09-28 15:48:18 -0400

    added the get_file_content to read content of files

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+from .config import *
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_file.startswith(abs_working_dir):
+        f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        f'Error: File not found or is not a regular file: "{file_path}'
+    
+    try:
+        with open(target_file, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        # if the file has another character after the first 10000, add the note at the end
+            if f.read(1):
+                truncation_message = f'\n[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                file_content_string += truncation_message
+        
+        return file_content_string
+    
+    except FileNotFoundError:
+        return f"Error: The file was not found at {file_path}"
+    except Exception as e:
+        return f"Error: Failure to open or read file {file_path}. Reason: {e}"
